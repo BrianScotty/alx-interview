240dba657d8a7b0abe01d56b061dd5db0b534998
BrianScotty <scotty.brianleong@gmail.com>
2024-12-12 18:29:14 +0000

    corrections

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,77 +1,79 @@
 #!/usr/bin/python3
-""" This is a bad way to get island perimeter """
+"""Module for Prime Game"""
 
 
-def island_perimeter(grid):
-    """ Islands perimeter and i am ashamed"""
-    sum = 0
-    for i in range(len(grid)):
-        for x in range(len(grid[i])):
-            if grid[i][x] == 1 and (i < (len(grid) - 1) and i > 0):
-                fb_ver2 = (grid[i + 1][x] != 1 or grid[i - 1][x] != 1)
-                fb_ver_n = (grid[i + 1][x] != 1 and grid[i - 1][x] != 1)
-                fb_ver = (grid[i + 1][x] == 1 and grid[i - 1][x] == 1)
-                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
-            elif grid[i][x] == 1 and (i == (len(grid) - 1)):
-                fb_ver2 = (grid[i - 1][x] != 1 or True)
-                fb_ver_n = (grid[i - 1][x] != 1 and True)
-                fb_ver = (grid[i - 1][x] == 1 and False)
-                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
-            elif grid[i][x] == 1 and (i == 0):
-                fb_ver2 = (True or grid[i + 1][x] != 1)
-                fb_ver_n = (True and grid[i + 1][x] != 1)
-                fb_ver = (False and grid[i + 1][x] == 1)
-                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
-    return sum
+def isWinner(x, nums):
+    """
+    Determines the winner of a set of prime number removal games.
+
+    Args:
+        x (int): The number of rounds.
+        nums (list of int): A list of integers where each integer n denotes
+        a set of consecutive integers starting from 1 up to and including n.
+
+    Returns:
+        str: The name of the player who won the most rounds (either "Ben"
+        or "Maria").
+        None: If the winner cannot be determined.
+
+    Raises:
+        None.
+    """
+    # Check for invalid input
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+    # Initialize scores and array of possible prime numbers
+    ben = 0
+    maria = 0
+    # Create a list 'a' of length sorted(nums)[-1] + 1 with all elements
+    # initialized to 1
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    # The first two elements of the list, a[0] and a[1], are set to 0
+    # because 0 and 1 are not prime numbers
+    a[0], a[1] = 0, 0
+    # Use Sieve of Eratosthenes algorithm to generate array of prime numbers
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+    # Play each round of the game
+    for i in nums:
+        # If the sum of prime numbers in the set is even, Ben wins
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    # Determine the winner of the game
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
 
 
-def total(grid, i, x, fb_ver, fb_ver2, fb_ver_n):
-    """ get total perimeter now """
-    sum = 0
-    if x == 0 and grid[i][x] == 1:
-        if fb_ver2 and (not fb_ver_n) and grid[i][x + 1] != 1:
-            sum += 3
-        elif fb_ver_n and grid[i][x + 1] != 1:
-            sum += 4
-        elif fb_ver_n and grid[i][x + 1] == 1:
-            sum += 3
-        elif fb_ver2 and (not fb_ver_n) and grid[i][x + 1] == 1:
-            sum += 2
-        elif fb_ver and grid[i][x + 1] != 1:
-            sum += 2
-        elif fb_ver and grid[i][x + 1] == 1:
-            sum += 1
-    elif (x != len(grid[i]) - 1) and grid[i][x] == 1:
-        fb_hor = (grid[i][x + 1] == 1 and grid[i][x - 1] == 1)
-        fb_hor_n = (grid[i][x + 1] != 1 and grid[i][x - 1] != 1)
-        fb_hor2 = (grid[i][x + 1] != 1 or grid[i][x - 1] != 1)
-        if (fb_hor2 and (not fb_hor_n)) and (fb_ver2 and (not fb_ver_n)):
-            sum += 2
-        elif (fb_hor2 and (not fb_hor_n)) and fb_ver_n:
-            sum += 3
-        elif (fb_hor2 and (not fb_hor_n)) and fb_ver:
-            sum += 1
-        elif fb_hor and fb_ver_n:
-            sum += 2
-        elif fb_hor and (fb_ver2 and (not fb_ver_n)):
-            sum += 1
-        elif fb_hor_n and fb_ver_n:
-            sum += 4
-        elif fb_hor_n and fb_ver:
-            sum += 2
-        elif fb_hor_n and (fb_ver2 and (not fb_ver_n)):
-            sum += 3
-    elif grid[i][x] == 1:
-        if fb_ver2 and (not fb_ver_n) and grid[i][x - 1] != 1:
-            sum += 3
-        elif fb_ver2 and (not fb_ver_n) and grid[i][x - 1] == 1:
-            sum += 2
-        elif fb_ver_n and grid[i][x - 1] != 1:
-            sum += 4
-        elif fb_ver_n and grid[i][x - 1] == 1:
-            sum += 3
-        elif fb_ver and grid[i][x - 1] != 1:
-            sum += 2
-        elif fb_ver and grid[i][x - 1] == 1:
-            sum += 1
-    return sum
+def rm_multiples(ls, x):
+    """
+    Removes multiples of a prime number from an array of possible prime
+    numbers.
+
+    Args:
+        ls (list of int): An array of possible prime numbers.
+        x (int): The prime number to remove multiples of.
+
+    Returns:
+        None.
+
+    Raises:
+        None.
+    """
+    # This loop iterates over multiples of a prime number and marks them as
+    # non-prime by setting their corresponding value to 0 in the input
+    # list ls. Starting from 2, it sets every multiple of x up to the
+    # length of ls to 0. If the index i * x is out of range for the list ls,
+    # the try block will raise an IndexError exception, and the loop will
+    # terminate using the break statement.
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
